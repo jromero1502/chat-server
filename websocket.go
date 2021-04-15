03a8e17d9cd7037aed2163b0192a4a70fb5172a1
jromero1502 <romerojulian115@gmail.com>
2021-04-14 21:36:46 -0500

    resolving bugs of socket

--- websocket.go
+++ websocket.go
@@ -8,46 +8,54 @@ import (
 )
 
 type WebSocket struct {
-	incommingMessages    chan []byte
-	incommingConnections chan *websocket.Conn
-	connections          []*websocket.Conn
+	connections []*websocket.Conn
 }
 
 var upgrader = websocket.Upgrader{}
 
 func NewWebSocket() *WebSocket {
 	ws := &WebSocket{
-		make(chan []byte),
-		make(chan *websocket.Conn),
 		make([]*websocket.Conn, 0),
 	}
 	return ws
 }
 
-func (ws *WebSocket) HandleMessage(currentConn *websocket.Conn) {
+func (ws *WebSocket) HandleMessage(stateChannel chan bool, connChannel chan *websocket.Conn, msgChannel chan []byte, currentConn *websocket.Conn) {
 	for {
 		select {
-		case conn := <-ws.incommingConnections:
+		case conn := <-connChannel:
 			fmt.Println("New connection")
 			ws.connections = append(ws.connections, conn)
-		case msg := <-ws.incommingMessages:
+		case msg := <-msgChannel:
 			PrintServerInfo("New message incomming. " + string(msg))
 			currentConn.WriteMessage(websocket.TextMessage, []byte("He recibido tu mensaje :)"))
+		case state := <-stateChannel:
+			if !state {
+				return
+			}
 		}
 	}
 }
 
 func (ws *WebSocket) Listen(w http.ResponseWriter, request *http.Request) {
+	if !upgrader.CheckOrigin(request) {
+		w.WriteHeader(404)
+		return
+	}
 	conn, _ := upgrader.Upgrade(w, request, nil)
-	go ws.HandleMessage(conn)
+	connChannel := make(chan *websocket.Conn)
+	msgChannel := make(chan []byte)
+	stateChannel := make(chan bool)
+	go ws.HandleMessage(stateChannel, connChannel, msgChannel, conn)
 	defer conn.Close()
-	ws.incommingConnections <- conn
+	connChannel <- conn
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			PrintServerInfo("Conexion de socket cerrada")
 			return
 		}
-		ws.incommingMessages <- msg
+		msgChannel <- msg
 	}
+	stateChannel <- false
 }
